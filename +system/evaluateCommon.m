cfc607e3ed208375c4790096751cc92ad79bea7e
yuorigane@gmail.com <origane.y.aa@m.titech.ac.jp>
2024-02-08 18:27:41 +0100

    able to use payfor

--- +system/evaluateCommon.m
+++ +system/evaluateCommon.m
@@ -14,23 +14,26 @@ function [q,x,eval_result,grad,dist,dist_gnd] = evaluateCommon(us,xd,Q,R,P,param
     end
     %[param_nominal,W] = system.makeUncertainty(seed_list(1), param_base, true); % calc nominal parameters
     [q_nominal,~,~] = system.steps(param_nominal.q0,u,param_nominal,opt_cnt,W_nominal); % calc nominal values
+    
+    L = zeros(1,Ns);
 
-    for i = 1:Ns
+    parfor i = 1:Ns
         %[param_unc,W] = system.makeUncertainty(seed, param_base, false); % calc uncertained parameters
-        if param_base.low_side_controller.average == "none"
+        if param_sets(i).low_side_controller == "none"
             [q(:,:,i),~,u_use] = system.steps(param_sets(i).q0,u,param_sets(i),opt_cnt,W_sets(i,:)); % steps openloop
         else
             [q(:,:,i),~,u_use] = system.stepsFB(param_sets(i).q0,q_nominal,u,param_sets(i),opt_cnt,W_sets(i,:));  % steps with feedback
         end
         param = param_sets(i);
-        x(:,:,i) = system.changeCoordinate(q(:,:,i),param);   % output variables
-        dist(i,:) = vecnorm(x([1,3],:,i)-param.obs_pos,2,1)-param.obs_size;
-        dist_gnd(i,:) = param.ground_depth-x(3,:,i);
-        
-        L = param_nominal.dt*dot(R*u_use(:,:),u_use(:,:)) + param_nominal.dt*dot(Q*(x(:,:,i)-xd(:,1)),(x(:,:,i)-xd(:,1)));
-        eval_result = eval_result + sum(L(:,:))+(x(:,end,i)-xd(:,1)).'*P*(x(:,end,i)-xd(:,1));
-        grad = grad + param_nominal.dt*(R*u_use(:,:));  % if Q and P = 0 only case
+        x_ = system.changeCoordinate(q(:,:,i),param);   % output variables
+        dist(i,:) = vecnorm(x_([1,3],:)-param.obs_pos,2,1)-param.obs_size;
+        dist_gnd(i,:) = param.ground_depth-x_(3,:);
+        x(:,:,i) = x_;
+        L(1,i) = sum(param_sets(i).dt*dot(R*u_use(:,:),u_use(:,:)));% + param_sets(i).dt*dot(Q*(x(:,:,i)-xd(:,1)),(x(:,:,i)-xd(:,1))));
     end
+
+    eval_result = sum(L(:,:))+sum(dot(pagemtimes(P,(x(:,end,:)-xd(:,1))), x(:,end,:)-xd(:,1)));
+    grad = 0;%grad + param_sets(i).dt*(R*u_use(:,:));  % if Q and P = 0 only case
 end
 
 
